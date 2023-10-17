f10be472eefdfe69f82656d8abac34ec4c5d37f2
Edgar Nasarenko <Edgar.Nasarenko@sunzinet.com>
2023-10-17 11:47:47 +0200

    Keine Ahnung, zu lange her

--- api/api.py
+++ api/api.py
@@ -1,12 +1,14 @@
 import flask
 from flask import request   # wird benötigt, um die HTTP-Parameter abzufragen
 from flask import jsonify   # übersetzt python-dicts in json
-from . import create_app
 
-app = create_app
+import sqlite3
+
+app = flask.Flask(__name__)
+app.config["DEBUG"] = True  # Zeigt Fehlerinformationen im Browser, statt nur einer generischen Error-Message
 
 @app.route('/', methods=['GET'])
 def reserveTable():
-    return "Hello"    
+    return "<h1>Tischreservierung</h1>"
 
 app.run()
